cc62055b6d7198791d328d87d9c075adfd35c219
keredu <kerwindepaz@gmail.com>
2022-04-12 15:16:02 -0700

    Testing smargit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ncurses.h>
+#include <string>
+
+#define BOARD_DIM 20
+#define BOARD_ROWS BOARD_DIM
+#define BOARD_COL BOARD_DIM * 2.5
+
+int main(int argc, char **argv){
+    initscr();
+    refresh();
+    noecho();
+    curs_set(0);
+    
+	endwin();
+
+	return 0;
+}
